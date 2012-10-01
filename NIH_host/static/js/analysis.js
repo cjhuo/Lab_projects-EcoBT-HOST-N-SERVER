84a926915beb623c87aa418945c824271f95e7ff
chengjia.huo <chengjia.huo@gmail.com>
2012-10-01 14:48:32 -0700

    add tooltip

--- NIH_host/static/js/analysis.js
+++ NIH_host/static/js/analysis.js
@@ -148,6 +148,7 @@ $(function () {
 	        	
 				/* re-plot everything */
 	            plot = $.plot(diagram, data, options);
+	            addHoverEvent();
 	           	plotOverview(data);
 	           	addSelectionEvent(data);
 	            addClickEvent();
@@ -296,6 +297,44 @@ $(function () {
 				plotAccordingToChoices();				
  		});
     }
+    
+    function showTooltip(x, y, contents) {
+        $('<div id="tooltip">' + contents + '</div>').css( {
+            position: 'absolute',
+            display: 'none',
+            top: y + 5,
+            left: x + 5,
+            border: '1px solid #fdd',
+            padding: '2px',
+            'background-color': '#fee',
+            opacity: 0.80
+        }).appendTo("body").fadeIn(200);
+    }
+
+    var previousPoint = null;
+    function addHoverEvent() {
+        diagram.bind("plothover", function (event, pos, item) {
+
+            if (item) {
+                if (previousPoint != item.dataIndex) {
+                    previousPoint = item.dataIndex;
+                    
+                    $("#tooltip").remove();
+                    var x = item.datapoint[0].toFixed(2),
+                        y = item.datapoint[1].toFixed(2);
+                    
+                    showTooltip(item.pageX, item.pageY,
+                    		"(" + x + ", " + y + ") of " + item.series.label);
+                }
+            }
+            else {
+                $("#tooltip").remove();
+                previousPoint = null;            
+            }
+
+        });
+    }
+
     
     getAndProcessData();
 });
