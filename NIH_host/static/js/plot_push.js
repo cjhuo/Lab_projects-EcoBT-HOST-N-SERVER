5724a18c54c6b0a2395b087ec06d3ad6eaa89cc5
chengjia.huo <chengjia.huo@gmail.com>
2012-11-04 14:07:28 -0800

    fix a reconnection problem when server is down; fix a duplicated sockets problem when server is down and back meanwhile browser is offline and back

--- NIH_host/static/js/plot_push.js
+++ NIH_host/static/js/plot_push.js
@@ -1,4 +1,5 @@
-/* Main script for generating plot.
+/**
+ * Main script for generating plot.
  *
  * .with push technology
  *
@@ -12,6 +13,12 @@ $(function () {
 	var socket = null; //websocket object
 	
 	var reconMsg = null; //reconnect div object
+	
+	/**
+	 * use to store reconnect procedure, to make sure there is only 1 websocket to server generated
+	 * not thread safe!!!TBD
+	 */
+	var reconn = null; 
 	
 	function initData(){
 		for (var i = 0; i < total_points; i++){
@@ -80,16 +87,55 @@ $(function () {
 	}
 	
 	function establishConnection() {
+		/*
 		if(socket != null){
 			socket.close();
 			socket = null;
 		}
+		*/
 	    socket = new WebSocket(url);
 	    socket.onmessage = function(event) {
 	    	onDataReceived($.parseJSON(event.data));
 	    	plot.setData([ dataToRes() ]);
 			plot.setupGrid();
 			plot.draw();
+	    };
+	    socket.onerror = function(event) {
+	    	alert('Error, readyState code is: ' + socket.readyState);
+	    	socket.close();
+	    	establishConnection();
+	    };
+	    
+	    socket.onclose = function(event) {
+	    	alert(socket.readyState);
+		    //var t = setInterval(function() {//check if connection is lost, for the case when server is down
+				//if(socket.readyState == 2 || socket.readyState == 3){ //connection is closed or closing
+			
+	    	/**
+			 * try to reconnect when connection is closed and if reconnection has been 
+			 * issued by other functions such as 'online' event handler then skip to prevent
+			 * duplication of socket
+			 * 
+			 * */
+	    	if(socket.readyState == 3){ //
+				hideReconMsg();
+				showReconMsg('connection reset by server, reconnecting in 5 secs...');
+				if(reconn == null){
+					reconn = setTimeout(function() {
+						establishConnection();
+						hideReconMsg();
+						reconn = null;
+						//alert('Network is back, readyState is: ' + socket.readyState);
+					}, 5000);
+				}
+			}
+			/*
+			else if(socket.readyState == 1) {
+				clearInterval(t);
+				hideReconMsg();
+			}
+			*/			
+		    //}, 6000);
 	    };
 	}
 
@@ -111,50 +157,14 @@ $(function () {
 			reconMsg = null;
 		}
 	}
+	
 	function update(){
-		if (navigator.onLine) { //navigator.onLine supports limited browser, see https://developer.mozilla.org/en-US/docs/DOM/window.navigator.onLine
+		if (navigator.onLine) { //navigator.onLine supports limited browsers, see https://developer.mozilla.org/en-US/docs/DOM/window.navigator.onLine
 			establishConnection();	
-			/*
-		    setInterval(function() {//check if connection is lost
-				if(socket.readyState == 2 || socket.readyState == 3){ //connection is closed or closing
-					showReconMsg();
-					establishConnection(socket);
-				}
-				else{
-					hideReconMsg();
-				}			
-		    }, 2000);
-		    */
 		}
 		else {
 			showReconMsg('brower is offline, check wifi...');
 		}
-	    
-	    /*
-		$.ajax({
-			url: dataurl,
-			cache: false,
-			method: 'GET',
-			dataType: 'json',
-			success: onDataReceived
-		});
-
-		plot.setData([ dataToRes() ]);
-		*/
-		/*
-		range = yaxisRange();
-		var opts = plot.getOptions();
-		for (var i = 0; i< opts.yaxes.length; i++){
-			opts.yaxes[i].min = range[0];
-			opts.yaxes[i].max = range[1];
-		}
-		*/
-	    /*
-		plot.setupGrid();
-		plot.draw();
-
-		timeVar = setTimeout(update, updateInterval);
-		*/
 	}
 	
 	$(window).bind('load', function(e) {
@@ -162,16 +172,50 @@ $(function () {
 		update();
 	});
 	
-	$(window).bind('online', function(e) {
+	$(window).bind('online', function(e) {		
+		if(socket != null)
+			alert('Network is back, readyState is: ' + socket.readyState);
+		/*
+		if(socket != null){
+			socket.close();
+			//socket = null;
+		}
+		*/
 		hideReconMsg();
-		showReconMsg('connection is back, connecting server...');
-		establishConnection();
-		hideReconMsg();
-		
+		showReconMsg('connection is back, connecting server in 5 secs...');
+		if(reconn == null) {
+			reconn = setTimeout(function() {
+				if(socket.readyState == 3){
+					establishConnection();
+					//alert('Network is back, readyState is: ' + socket.readyState);
+					hideReconMsg();
+					reconn = null;
+				}
+			}, 5000);
+		}
 	});
 	
 	$(window).bind('offline', function(e) {
+		/**
+		 * firing a close() cause a connection to close or to timeout on user browser's side after 
+		 * 300secs by default; WebSocket on tornado server will also be closed or timed out.
+		 * 
+		 * This is just for the purpose of setting socket.readyState to 'CLOSED' in order to garbage 
+		 * collect old socket and generate new socket next time when browser is online.
+		 * This is used for the case of lost connection when browser waked up from sleep and timed 
+		 * out at server side. if close() is not issued, when browser is back from sleep, readyState 
+		 * will still be 'OPEN'. Connection lost will only be detected unless after 300secs or 
+		 * (by sending to server something such as close(), send() I guess...).
+		 * 
+		 * The close is actually issued when browser is back from wake since there is no way close
+		 * request can be sent to server when offline 
+		 * 
+		 * This is only a workaround since there is no way whether the connection still exist even if
+		 * websocket.readyState on browser side is 'OPEN'. A short ping-pong mechanism might resolve 
+		 * the issue.
+		 * */
+		socket.close();
 		hideReconMsg();
-		showReconMsg('connection lost...')
+		showReconMsg('connection lost, check wifi...')
 	});
 });
