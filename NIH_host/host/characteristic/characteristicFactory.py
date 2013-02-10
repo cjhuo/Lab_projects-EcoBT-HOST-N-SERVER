8984e645b0b12d78642b92daae8d62ad61c5211f
chengjia.huo <chengjia.huo@gmail.com>
2013-02-10 00:57:28 -0800

    add factory pattern to implement characteristic; and lots of other works...

---
+++ NIH_host/host/characteristic/characteristicFactory.py
@@ -0,0 +1,35 @@
+'''
+Created on Feb 9, 2013
+
+@author: cjhuo
+'''
+from implementation import DeviceInfo, ACCXYZ, ACCEnable, \
+SIDsEnable, SIDsRate, SIDsStart,\
+SIDsTempRead, SIDsHumidRead, EPL_LED
+
+def createCharacteristic(UUID, instance, peripheral):
+    c = None
+    if UUID == "2A23":
+        c = DeviceInfo.DeviceInfo()
+    elif UUID == "FFA6":
+        c = ACCXYZ.ACCXYZ()
+    elif UUID == "FFA1":
+        c = ACCEnable.ACCEnable()
+    elif UUID == "FE11":
+        c = SIDsEnable.SIDsEnable()
+    elif UUID == "FE12":
+        c = SIDsRate.SIDsRate()
+    elif UUID == "FE13":
+        c = SIDsStart.SIDsStart()
+    elif UUID == "FE14":
+        c = SIDsTempRead.SIDsTempRead()
+    elif UUID == "FE15":
+        c = SIDsHumidRead.SIDsHumidRead()
+    elif UUID == "FF11" or UUID == "FF12" or UUID == "FF13" or UUID == "FF14":
+        c = EPL_LED.EPL_LED()
+                
+    c.setUUID(UUID)  
+    c.setInstance(instance)      
+    c.setPeripheral(peripheral)   
+    return c    
+    
---
+++ NIH_host/host/characteristic/implementation/Characteristic.py
@@ -0,0 +1,26 @@
+'''
+Created on Feb 9, 2013
+
+@author: cjhuo
+
+@summary:  interface of Characteristic
+'''
+
+class Characteristic(object):
+    def __init__(self):
+        self.UUID = None
+        self.instance = None
+        self.privilege = None   # 0: Read Only; 1: Read and Write; 
+                                # 2: Read Only but need to be read at the beginning(e.g. System info)
+        
+    def setUUID(self, UUID):
+        self.UUID = UUID
+    
+    def setInstance(self, instance):
+        self.instance = instance
+        
+    def setPeripheral(self, peripheral):
+        self.peripheral = peripheral
+        
+    def process(self):
+        pass
