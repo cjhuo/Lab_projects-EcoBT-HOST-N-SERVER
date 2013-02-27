11874758f024780a1d58d9a1e7553386db777e2f
chengjia.huo <chengjia.huo@gmail.com>
2013-02-27 14:04:18 -0800

    temporarily fix problem on ecg by choose any channel to analyze

--- ECG/webEngine/ecg/ECG.py
+++ ECG/webEngine/ecg/ECG.py
@@ -2,7 +2,7 @@ __author__ = 'leetop'
 import scipy
 import scipy.signal
 import numpy
-import pylab
+
 
 class QrsDetectionError(Exception):
     def __init__(self, value):
@@ -17,15 +17,15 @@ class Ecg() :
         self._readinfo()
         self.data = numpy.array(ecgdata).transpose()
 
-        if len(self.data) == 1:
+        if len(self.data)==1:
             self.data = numpy.array([self.data]).transpose()
 
     def _readinfo(self):
         """Read the info and fill missing values with defaults
         """
         # defaults
-        self.name = ''
-        self.age = 0
+        self.name =  ''
+        self.age =  0
         self.sex = 'u'
         self.samplingrate = 200
 
@@ -38,7 +38,7 @@ class Ecg() :
                 variable = self.infodict[key]
             except KeyError:
                 self._warning("Info does not contain %s, replacing with %s"
-                % (key, variable))
+                %(key, variable))
 
     def _warning(self, msg):
         print msg
@@ -46,10 +46,10 @@ class Ecg() :
     def bpfilter(self, ecg):
         Nyq = self.samplingrate / 2
 
-        wn = [5.0 / Nyq, 15.0 / Nyq]
-        b, a = scipy.signal.butter(3, wn, btype='bandpass')
+        wn = [5.0/ Nyq, 15.0 / Nyq]
+        b,a = scipy.signal.butter(3, wn, btype = 'bandpass')
 
-        return scipy.signal.filtfilt(b, a, ecg)
+        return scipy.signal.filtfilt(b,a,ecg)
 
     def mw_integrate(self, ecg):
 
@@ -58,7 +58,7 @@ class Ecg() :
         int_ecg = scipy.zeros_like(ecg)
         cs = ecg.cumsum()
 
-        int_ecg[window_length:] = (cs[window_length:] - 
+        int_ecg[window_length:] = (cs[window_length:] -
                                    cs[:-window_length]) / window_length
         int_ecg[:window_length] = cs[:window_length] / scipy.arange(
             1, window_length + 1)
@@ -70,11 +70,11 @@ class Ecg() :
         if len(self.data.shape) == 1:
             self.raw_ecg = self.data
         else:
-            self.raw_ecg = self.data[:, qrslead]
+            self.raw_ecg = self.data[:,qrslead]
 
         self.filtered_ecg = self.bpfilter(self.raw_ecg)
-        self.diff_ecg = scipy.diff(self.filtered_ecg)
-        self.sq_ecg = (self.diff_ecg) * (self.diff_ecg.transpose())
+        self.diff_ecg  = scipy.diff(self.filtered_ecg)
+        self.sq_ecg = (self.diff_ecg)*(self.diff_ecg.transpose())
         self.int_ecg = self.mw_integrate(self.sq_ecg)
 
         # Construct buffers with last 8 values
@@ -94,7 +94,7 @@ class Ecg() :
     def _initializeBuffers(self, ecg):
         srate = self.samplingrate
 
-        self.signal_peak_buffer = [max(ecg[start * srate:start * srate + srate])
+        self.signal_peak_buffer = [max(ecg[start*srate:start*srate+srate])
                                    for start in range(8)]
         self.noise_peak_buffer = [0] * 8
         self.rr_buffer = [1] * 8
@@ -107,8 +107,8 @@ class Ecg() :
 
     def peakDetect(self, ecg):
 
-        all_peaks = [i for i in range(1, len(ecg) - 1)
-                     if ecg[i - 1] < ecg[i] > ecg[i + 1]]
+        all_peaks = [i for i in range(1,len(ecg)-1)
+                     if ecg[i-1] < ecg[i] > ecg[i+1]]
         peak_amplitudes = [ecg[peak] for peak in all_peaks]
 
         final_peaks = []
@@ -145,10 +145,10 @@ class Ecg() :
             if amplitude > self.threshold:
                 self.acceptasQRS(peak, amplitude)
 
-            elif amplitude > self.threshold / 2 and len(self.QRSpeaks) > 0 and len(peaks) > index + 1:
+            elif amplitude > self.threshold/2 and len(self.QRSpeaks) > 0 and len(peaks) > index+1:
                 meanrr = scipy.mean(self.rr_buffer)
                 lastQRSms = (peak - self.QRSpeaks[-1]) * (1000 / self.samplingrate)
-                lastQRS_to_next_peak = peaks[index + 1] - self.QRSpeaks[-1]
+                lastQRS_to_next_peak = peaks[index+1] - self.QRSpeaks[-1]
 
                 if lastQRSms > 360 and lastQRS_to_next_peak > 1.5 * meanrr:
                     self.acceptasQRS(peak, amplitude)
@@ -175,47 +175,3 @@ class Ecg() :
     def acceptasNoise(self, peak, amplitude):
         self.noise_peak_buffer.pop(0)
         self.noise_peak_buffer.append(amplitude)
-
-    def visualize_qrs_detection(self, savefilename=False):
-        ecglength = len(self.raw_ecg)
-        ten_seconds = 10 * self.samplingrate
-
-        if ecglength > ten_seconds:
-            segmentend = ten_seconds
-        elif ecglength < ten_seconds:
-            segmentend = ecglength
-
-        segmentQRSpeaks = [peak for peak in self.QRSpeaks if peak < segmentend]
-
-
-
-        pylab.figure()
-        pylab.subplot(611)
-        pylab.plot(self.raw_ecg[:segmentend])
-        pylab.ylabel('Raw ECG', rotation='horizontal')
-        pylab.subplot(612)
-        pylab.plot(self.filtered_ecg[:segmentend])
-        pylab.ylabel('Filtered ECG', rotation='horizontal')
-        pylab.subplot(613)
-        pylab.plot(self.diff_ecg[:segmentend])
-        pylab.ylabel('Differential', rotation='horizontal')
-        pylab.subplot(614)
-        pylab.plot(self.sq_ecg[:segmentend])
-        pylab.ylabel('Squared differential', rotation='horizontal')
-        pylab.subplot(615)
-        pylab.plot(self.int_ecg[:segmentend])
-        pylab.ylabel('Integrated', rotation='horizontal')
-        pylab.subplot(616)
-        pylab.hold(True)
-        pylab.plot(self.raw_ecg[:segmentend])
-#        pylab.stem(segmentQRSpeaks, self.raw_ecg[segmentQRSpeaks], markerfmt='ro', basefmt='', linefmt='')
-        testtest = []
-        pylab.stem(testtest, self.raw_ecg[testtest], markerfmt='ro', basefmt='', linefmt='')
-        pylab.hold(False)
-        pylab.ylabel('QRS peaks', rotation='horizontal')
-
-        if savefilename:
-            pylab.savefig(savefilename)
-        else:
-            pylab.show()
-
