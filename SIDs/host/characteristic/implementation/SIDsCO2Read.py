9172dbdc3f80e439454f731f4fd1e893a1bc7a52
Brett Chien <brett.chien@gmail.com>
2013-03-03 19:24:57 -0800

    reading conversion complete

--- SIDs/host/characteristic/implementation/SIDsCO2Read.py
+++ SIDs/host/characteristic/implementation/SIDsCO2Read.py
@@ -36,19 +36,24 @@ class SIDsCO2Read(Characteristic):
     def __init__(self):
         Characteristic.__init__(self)
         self.privilege = 0
-        
+
     def process(self):
         hex_str = binascii.hexlify(self.instance._.value)
         print "CO2 READING: ", hex_str
         value = self.instance._.value
-        hour, min, sec, LED00, LED01, LED10, LED11, amb0, amb1, rh, temp = struct.unpack("<BBBhhhhhhhh", value)
-        
+        hour, min, sec, LED00, LED01, LED10, LED11, amb0, amb1, rh, temp = struct.unpack("<BBBhhhhhhHH", value)
+        print rh, temp
+        rh = rh & 0xFFFC
+        rh = -6.0 + (125.0 * rh) / 65536
+        temp = temp & 0xFFFC
+        temp = -46.85 + (175.72 * temp) / 65536
+
         print hour, min, sec, LED00, LED01, LED10, LED11, amb0, amb1, rh, temp
         '''
         self.start = int(hex_str, base=16) # 1: enabled; 0: disabled
         print "SIDS SHT25 Start?(%s) %d" % (self.instance._.UUID, self.start)
         data = {
-                'type': 'SIDsStart', 
+                'type': 'SIDsStart',
                 'value': self.start,
                 'uuid': self.UUID
                 }
@@ -57,10 +62,10 @@ class SIDsCO2Read(Characteristic):
 
     def createStartFlag(self):
         return self.createFlag(1)
-        
+
     def createStopFlag(self):
         return self.createFlag(0)
-        
+
     def createFlag(self, flag):
         byte_array = array.array('b', chr(flag))
         val_data = NSData.dataWithBytes_length_(byte_array, len(byte_array))
