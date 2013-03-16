df7e3f31036d8c8cb73ecf4767e4f8bc6a07a570
chengjia.huo <chengjia.huo@gmail.com>
2013-03-15 23:10:23 -0700

    remove ecg modules from SIDs

--- SIDs/webEngine/webApp.py
+++ SIDs/webEngine/webApp.py
@@ -2,8 +2,6 @@ import os.path
 
 import tornado.web
 
-from webHandlers.fake.fakePlotHandler import *
-from webHandlers.ECGHandler import *
 from webHandlers.pageRenderingHandlers import *
 from webHandlers.AuthHandlers import AuthHandler
 from webHandlers.ConfigFileHandlers import *
@@ -12,35 +10,22 @@ from host.Sockets import Sockets
 from host.EcoBTWebSocket import EcoBTWebSocket
 
 from db.Models import DataSource, Device, DataLog
-from ecg.ECG_reader import ECG_reader
 
 class Application(tornado.web.Application):
     def __init__(self, ecoBTApp):
         self.ds = DataSource()
-        self.ecg = ECG_reader()
         self.globalSockets = Sockets()
         self.ecoBTApp = ecoBTApp
         
         self.handlers = [
                 (r'/', MainHandler),          
                 (r'/soundMonitor', SoundMonitorHandler),
-                (r'/ecgHandler', ECGHandler, dict(ecg = self.ecg)),
-                (r'/ecgAllInOne', ECGAllInOneHandler),
-                (r'/cardReader', CardReaderHandler),
-                (r'/tempAnalysis', TempAnalysisHandler),
-                (r'/analysis_old', AnalysisOldHandler),
-                (r'/analysis_allInOne', AnalysisAllInOnceHandler),            
+                (r'/tempAnalysis', TempAnalysisHandler),           
                 (r'/orientation', OrientationHandler),
                 (r'/temperature', TemperatureHandler),
-                (r'/liveECG', LiveECGHandler),
                 (r'/liveSIDs', LiveSIDsHandler),
                 (r'/sidsAll', SIDsAllHandler),
                 (r'/administration', AdministrationHandler),
-                (r'/plot', PlotHandler),
-                (r'/analysis', AnalysisHandler),
-                (r'/point', PointHandler, dict(ds = self.ds)),
-                #(r'/dsp', DSPHandler, dict(ecg = self.ecg)),
-                (r"/fakeSocket", ClientSocket, dict(ds = self.ds)),
                 (r"/login", AuthHandler),
                 (r"/logout", LogOutHandler),
                 (r"/socket", EcoBTWebSocket, dict(globalSockets = self.globalSockets, 
