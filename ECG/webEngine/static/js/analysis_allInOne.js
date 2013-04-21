82864202db6ac63bdf1dd043a80c1eee9942d3ad
chengjia.huo <chengjia.huo@gmail.com>
2013-04-20 19:42:48 -0700

    adjust css for touch device compatibility

--- ECG/webEngine/static/js/analysis_allInOne.js
+++ ECG/webEngine/static/js/analysis_allInOne.js
@@ -522,8 +522,10 @@ $(function () {
 		else
 			diagramLength = diagramLength.toString() + 'px';
     	resizer = $('<div id="resizer" />').css( {
-            width: diagramLength,
+            //width: diagramLength,
+            width: '100%',
             minHeight: '400px',
+            margin: 'auto',
             //border: '1px solid silver'
 
         });	
@@ -843,11 +845,11 @@ $(function () {
         			millisecond: '%H:%M:%S',
     	        	second: '%H:%M:%S',
     	        	minute: '%H:%M:%S',
-    	        	hour: '%H:%M',
-    	        	day: '%e. %b',
-    	        	week: '%e. %b',
-    	        	month: '%b \'%y',
-    	        	year: '%Y'
+    	        	hour: '%H:%M:%S',
+    	        	day: '%H:%M:%S',
+    	        	week: '%H:%M:%S',
+    	        	month: '%H:%M:%S',
+    	        	year: '%H:%M:%S'
     	        },
     	        
     	        labels: {
