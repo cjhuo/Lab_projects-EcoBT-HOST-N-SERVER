7b97aaeeb8e78915e6d0f8e4d2dbf365867e2f17
chengjia.huo <chengjia.huo@gmail.com>
2013-06-06 16:26:11 -0700

    modify characteristics loading approach with Factory

--- host/characteristic/characteristicFactory.py
+++ host/characteristic/characteristicFactory.py
@@ -4,42 +4,17 @@ Created on Feb 9, 2013
 @author: cjhuo
 '''
 from IOBluetooth import *
-from host.characteristic.implementation import *
+
+# dynamically load modules from implementation package
+import os
+for module in os.listdir(os.path.join(os.path.dirname(__file__), "implementation")):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    exec("from implementation import %s" % module[:-3])
+del module
 
 def createCharacteristic(UUID, instance, service, peripheralWorker):
     c = None
-    '''
-    if UUID == "2A23":
-        c = DeviceInfo.DeviceInfo() 
-    elif UUID == "FFA6":
-        c = ACCXYZ.ACCXYZ()
-    elif UUID == "FFA1":
-        c = ACCEnable.ACCEnable()
-    elif UUID == "FE11":
-        c = SIDsCO2Status.SIDsCO2Status()
-    elif UUID == "FE12":
-        c = SIDsCO2Set.SIDsCO2Set()
-    elif UUID == "FE13":
-        c = SIDsCO2Read.SIDsCO2Read()
-    elif UUID == "FE14":
-        c = SIDsTempRead.SIDsTempRead()
-    elif UUID == "FE15":
-        c = SIDsHumidRead.SIDsHumidRead()
-    elif UUID == "FF11" or UUID == "FF12":
-        c = LEDEnable.LEDEnable()
-    elif UUID == "FF13" or UUID == "FF14":
-        c = LEDBlinkInterval.LEDBlinkInterval()
-    elif UUID == "FF21":
-        c = RTCSet.RTCSet()
-    elif UUID == "FF22":
-        c = RTCGet.RTCGet()  
-    elif UUID == "FEC2":
-        c = ECGStatus.ECGStatus()    
-    elif UUID == "FEC5":
-        c = ECGSet.ECGSet() 
-    elif UUID == "FEC6" or UUID == "FEC7":
-        c = ECGGet.ECGGet()     
-    '''
     if UUID in ProfileDict.keys():
         objStr = ProfileDict[UUID] + '.' + ProfileDict[UUID] + '()'
         c = eval(objStr)
--- host/characteristic/implementation/__init__.py
+++ host/characteristic/implementation/__init__.py
@@ -1,4 +1,6 @@
+'''
 __all__ = ['Characteristic', 'DeviceInfo', 'ACCXYZ', 'ACCEnable', 
            'SIDsCO2Status', 'SIDsCO2Set', 'SIDsCO2Read', 'SIDsTempRead', 
            'SIDsHumidRead', 'LEDEnable', 'LEDBlinkInterval',
            'RTCSet', 'RTCGet', 'ECGGet', 'ECGSet', 'ECGStatus']
+'''
