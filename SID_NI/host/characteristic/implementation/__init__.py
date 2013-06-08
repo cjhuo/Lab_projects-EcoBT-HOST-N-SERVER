ae66bd70f22abff10da4f88b99971d960ba8adfc
chengjia.huo <chengjia.huo@gmail.com>
2013-06-07 19:59:17 -0700

    remove 'SIDs no idea' project since it conflicts with branch SIDs_ENG_JESUS

--- SID_NI/host/characteristic/implementation/__init__.py
+++
@@ -1,4 +0,0 @@
-__all__ = ['Characteristic', 'DeviceInfo', 'ACCXYZ', 'ACCEnable', 
-           'SIDsCO2Status', 'SIDsCO2Set', 'SIDsCO2Read', 'SIDsTempRead', 
-           'SIDsHumidRead', 'LEDEnable', 'LEDBlinkInterval',
-           'RTCSet', 'RTCGet', 'ECGGet', 'ECGSet', 'ECGStatus']
--- SID_NI/webEngine/db/DataSource.py
+++
@@ -1,4 +0,0 @@
-#MACROS Speicfy location of DB, relative path
-import os
-DB_PATH = 'sqlite:///' + os.path.join(os.path.dirname(__file__), 'NIH.db')
-
